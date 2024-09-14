72e0635ad23b108387bb2cd9cbbf35c5b33c2b73
MustafaAkolawala <mustafaakolawala@eng.rizvi.edu.in>
2024-09-14 16:19:32 +0530

    Initial Server Setup

---
+++ server/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/api")
+async def root() -> dict[str, str]:
+    """
+    Root endpoint of the ML Captcha Service.
+    """
+    return {"message": "Welcome to the ML Captcha Service"}
