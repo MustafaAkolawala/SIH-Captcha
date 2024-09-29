777080ab62c979c7c118c55fd6475530e2b6ec3d
smitjiwani <smitjiwani44@gmail.com>
2024-09-29 18:24:41 +0530

    shadcn error fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,9 @@
 import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    host: true,
     port: 8081,
     proxy: {
       "/api": "http://localhost:6500",
